3511610086f67ac335ba2d500d82a3ff55be0b51
Jeffry Leon <ryffje@gmail.com>
2025-05-14 11:14:54 -0600

    add change import Player in Gamets file

--- src/Game.ts
+++ src/Game.ts
@@ -1,4 +1,4 @@
-import { Player } from "./player";
+import { Player } from "./Player";
 import { GameMap } from "./Map";
 import { CellContentType } from "./Cell";
 import * as readline from 'readline';
