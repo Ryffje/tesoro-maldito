e80961e430a1b269ecb3e5c783202e8781264a58
Jeffry Leon <ryffje@gmail.com>
2025-05-26 13:56:53 -0600

    add change in the file-structure

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import * as readline from 'readline';
+import {Game} from './Game';
+
+const r1 = readline.createInterface ({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function Menu(){
+    console.log("\n 🏝️🏴‍☠️The Cursed treasure");
+    console.log("1. Start the adventure");
+    console.log("2. Instructions");
+    console.log("3. Exit \n");
+
+    r1.question("Take a option (1-3):", (option) =>{
+        switch (option.trim()) {
+            case '1':
+                console.log(" 🌴¡Launching the expedition!");
+            r1.close();
+            const game = new Game();
+            game.start();
+            break;
+            
+            case '2':
+                console.log()
+        }
+    })
+}
